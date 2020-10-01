660a91efd6c1b9bf409b0e3264e3d47d820f7f0f
Rishi <rishi.sisodiya@cocogeneralinsurance.com>
2020-10-01 10:11:47 +0530

    changes -added serializers -optimised query -different api for list and single movie retrive

--- movie_api/serializers.py
+++ movie_api/serializers.py
@@ -1,3 +1,4 @@
+from movie_api.models import *
 from rest_framework import serializers
 from django.contrib.auth import get_user_model # If used custom user model
 from django.contrib.auth.models import Permission
@@ -23,5 +24,116 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserModel
-        # Tuple of serialized model fields (see link [2])
+        # Tuple of serialized model 
         fields = ( "id", "email", "username", "password", )
+
+class MovieListSerializer(serializers.BaseSerializer):
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'movie_name': instance.name
+        }
+
+class GetMovieSerializer(serializers.BaseSerializer):
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "99popularity": instance._99popularity,
+            "imdb_score":instance.imdb_score,
+            "genre":[x.name for x in instance.genre.all()],
+            "director": instance.director,
+            "movie_name": instance.name
+        }
+class MovieSearchSerializer(serializers.BaseSerializer):
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "99popularity": instance._99popularity,
+            "imdb_score":instance.imdb_score,
+            "genre":[x.name for x in instance.genre.all()],
+            "director": instance.director,
+            "movie_name": instance.name
+        }
+class MovieSerializer(serializers.BaseSerializer):
+    def to_internal_value(self, data):
+        _99popularity  = data.get('99popularity')
+        genre = data.get('genre')
+        imdb_score = data.get('imdb_score')
+        director = data.get('director')
+        name = data.get('name')
+        if not _99popularity :
+            raise serializers.ValidationError({
+                '99popularity': 'This field is required.'
+            })
+        if not genre :
+            raise serializers.ValidationError({
+                'genre': 'This field is required.'
+            })
+        if not imdb_score :
+            raise serializers.ValidationError({
+                'imdb_score': 'This field is required.'
+            })
+        if not director :
+            raise serializers.ValidationError({
+                'director': 'This field is required.'
+            })
+        if not name :
+            raise serializers.ValidationError({
+                'name': 'This field is required.'
+            })
+        if imdb_score < 0 or imdb_score > 10:
+            raise serializers.ValidationError({
+                'imdb_score': 'Should be a number between 0 to 10'
+            })
+        if _99popularity < 0 or _99popularity > 100:
+            raise serializers.ValidationError({
+                '_99popularity': 'Should be a number between 0 to 100'
+            })
+        if type(genre) != list:
+            raise serializers.ValidationError({
+                'genre': 'Should be a list or array'
+            })
+
+        # # Return the validated values. This will be available as
+        # # the `.validated_data` property.
+        return {
+           "_99popularity":_99popularity,
+            "genre":genre,
+            "imdb_score" :imdb_score,
+            "director" :director,
+            "name":name
+        }
+
+    def to_representation(self, instance):
+        print("inside to represent",[x.name for x in instance.genre.all()])
+        return {
+            "_99popularity":instance._99popularity,
+            "genre":[x.name for x in instance.genre.all()],
+            "imdb_score":instance.imdb_score,
+            "director":instance.director,
+            "name":instance.name
+        }
+
+    def create(self, validated_data):
+        search_string = ' '
+        for i in validated_data.keys():
+            if i != "genre":
+                search_string = str(validated_data[i]) +  search_string
+        validated_data["search_string"] = search_string + (' ').join(validated_data["genre"])
+        genre = validated_data.pop("genre")
+        movie = Movie.objects.create(**validated_data)
+        movie.save()
+        print("movie saved: ", movie.id)
+
+        return movie
+
+    def update(self, instance, validated_data):
+        genre = validated_data.pop("genre")
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
+
+
+
+   
